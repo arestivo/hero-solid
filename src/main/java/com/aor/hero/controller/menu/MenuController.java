31e3bc7941a69d037d72e83f761bff070508b1f5
Andre Restivo <andre.restivo@gmail.com>
2021-05-10 15:54:01 +0100

    Fixed wrong import

--- src/main/java/com/aor/hero/controller/menu/MenuController.java
+++ src/main/java/com/aor/hero/controller/menu/MenuController.java
@@ -2,10 +2,10 @@ package com.aor.hero.controller.menu;
 
 import com.aor.hero.controller.Controller;
 import com.aor.hero.Game;
-import com.aor.hero.game.states.GameState;
 import com.aor.hero.gui.GUI;
 import com.aor.hero.model.game.arena.LoaderArenaBuilder;
 import com.aor.hero.model.menu.Menu;
+import com.aor.hero.states.GameState;
 
 import java.io.IOException;
 
