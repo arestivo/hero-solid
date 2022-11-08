2c332d4445e1cb9a78156ae8d0991ec05a521669
André Restivo <arestivo@carbon.local>
2022-11-08 18:50:04 +0000

    Changed FPS to 10

--- src/main/java/com/aor/hero/Game.java
+++ src/main/java/com/aor/hero/Game.java
@@ -27,7 +27,7 @@ public class Game {
     }
 
     private void start() throws IOException {
-        int FPS = 1;
+        int FPS = 10;
         int frameTime = 1000 / FPS;
 
         while (this.state != null) {
