46b2d3d8ed0d2e15ebb53d1e325a3a44928aabdc
Josh Wein <joshwein@rocketmail.com>
2023-11-20 22:53:23 -0500

    Add drawing test

---
+++ src/test/java/com/aor/hero/viewer/DoorViewerTest.java
@@ -0,0 +1,29 @@
+package com.aor.hero.viewer;
+
+import com.aor.hero.gui.GUI;
+import com.aor.hero.model.game.elements.Door;
+import com.aor.hero.model.game.elements.Hero;
+import com.aor.hero.viewer.game.DoorViewer;
+import com.aor.hero.viewer.game.HeroViewer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class DoorViewerTest {
+  private Door door;
+  private DoorViewer viewer;
+  private GUI gui;
+
+  @BeforeEach
+  void setUp() {
+    door = new Door(0, 0);
+    viewer = new DoorViewer();
+    gui = Mockito.mock(GUI.class);
+  }
+
+  @Test
+  void drawElement() {
+    viewer.draw(door, gui);
+    Mockito.verify(gui, Mockito.times(1)).drawDoor(door.getPosition());
+  }
+}
